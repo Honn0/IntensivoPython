cf22eebcc92d2c6230558018b9ba15f4ebebde69
Felipe Almeida <felipesouza.root@gmail.com>
2021-05-31 23:42:59 -0300

    subida pt1 cap8

---
+++ 8.1.py
@@ -0,0 +1,7 @@
+# Escreva uma função chamada display_message() que mostre uma frase informando a todos o que você
+# está aprendendo neste capítulo. Chame a função e certifique-se de que a mensagem seja exibida corretamente.
+
+def display_message():
+    print("Aprendendo funções no capítulo 8")
+
+display_message()
---
+++ greeter.py
@@ -0,0 +1,5 @@
+def greet_user(username):
+    """Exibe uma saudaçao simples."""
+    print("Hello, " + username.title() + "!")
+
+greet_user('jesse')
